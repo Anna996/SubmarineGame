447c89cc85ef596713b89f9e25912323afb54b21
anna aba <annaaba15@gmail.com>
2022-04-20 15:14:32 +0300

    update method: createPattern in Submarine class

--- SubmarineGameProject/src/Submarine/Submarine/Submarine.java
+++ SubmarineGameProject/src/Submarine/Submarine/Submarine.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Submarine {
 	private int size;
 	private char[][] pattern;
+	public static final char CHAR = '*' ;
 
 	public void createRandom(int maxSize) {
 		Random random = new Random();
@@ -24,9 +25,27 @@ public class Submarine {
 
 	// TODO Logic
 	private void createPattern() {
-
+		if (size == 1) {
+			pattern[0][0] = CHAR;
+			return;
+		}
+		
+		int[] randomElement = getRandomElement(size);
+		pattern[randomElement[0]][randomElement[1]] = CHAR;
+		int[][] neighbors = getNeighbors(randomElement);
+		
 	}
 	
+	// TODO
+	private int[] getRandomElement(int size) {
+		return null;
+	}
+	
+	// TODO
+	private int[][] getNeighbors(int[] source){
+		return null;
+	}
+
 	public char[][] getPattern() {
 		return pattern;
 	}
