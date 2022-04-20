97a5ab4fbad37dba46b31c8a08dfa65eab32a743
Hodayabe <odayab98@gmail.com>
2022-04-20 14:21:59 +0300

    init the boards

--- SubmarineGameProject/src/Submarine/board/Board.java
+++ SubmarineGameProject/src/Submarine/board/Board.java
@@ -6,5 +6,47 @@ public class Board {
 	private char[][] subBoard;
 	
 	
+	public Board() {
+		subBoard=new char[10][20];
+		for (int i = 0; i < subBoard.length; i++) {
+			for (int j = 0; j < subBoard.length; j++) {
+				subBoard[i][j] =' ';
+			}
+		}
+		
+		userBoard=new char[12][22];
+		for (int i = 0; i < userBoard.length; i++) {
+			for (int j = 0; j < userBoard.length; j++) {
+				if( i==0 || i==userBoard.length-1 || j==0 || j==userBoard.length-1)
+					userBoard[i][j] ='*';
+				else
+					userBoard[i][j] =' ';
+			}
+		}
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
